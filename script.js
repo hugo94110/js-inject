4f45ae8314d9868f5d43d0300476e5cc716f700b
Hugo <hugo.leveque@efrei.net>
2026-06-21 20:04:26 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
 (function() {
+    function openModal() {
+        if (document.querySelector('#os-modal')) return;
+
+        var modal = document.createElement('div');
+        modal.id = 'os-modal';
+        modal.className = 'FullModalOverlay';
+        modal.style.display = 'block';
+        modal.innerHTML = `
+            <div class="ModalOverlayContent ModalOverlayBackground"></div>
+            <div class="ModalPosition" tabindex="0">
+                <div class="ModalPosition_Content">
+                    <div class="ModalPosition_TopBar"></div>
+                    <div class="DialogContent _DialogLayout GenericDialogBase GenericConfirmDialog _DialogCenterVertically">
+                        <div class="DialogContent_InnerWidth">
+                            <div class="Panel">
+                                <div class="_2IWpfqj8UL5hUs7n-pxnUy DialogHeader">
+                                    <div role="heading" aria-level="2" class="DialogHeader">Installer un script</div>
+                                </div>
+                                <div class="DialogInput_Wrapper _DialogLayout Panel" style="margin-top:16px;">
+                                    <input id="os-appid-input"
+                                        class="DialogInput DialogInputPlaceholder DialogTextInputBase Focusable"
+                                        placeholder="AppID"
+                                        type="text"
+                                        spellcheck="false">
+                                </div>
+                                <div class="DialogFooter">
+                                    <div class="DialogTwoColLayout _DialogColLayout Panel">
+                                        <button id="os-download-btn" type="button" class="DialogButton _DialogLayout Primary Focusable">Télécharger</button>
+                                        <button id="os-cancel-btn" type="button" class="DialogButton _DialogLayout Secondary Focusable">Annuler</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        document.body.appendChild(modal);
+
+        modal.querySelector('.ModalOverlayContent').onclick = () => modal.remove();
+        modal.querySelector('#os-cancel-btn').onclick = () => modal.remove();
+        modal.querySelector('#os-download-btn').onclick = () => {
+            var appid = modal.querySelector('#os-appid-input').value.trim();
+            if (!appid) return;
+            console.log('OpenSteam: download for AppID', appid);
+            modal.remove();
+        };
+    }
+
     function inject() {
         var container = document.querySelector('._3cykd-VfN_xBxf3Qxriccm._1-9sir4j_KQiMqdkZjQN0u');
         if (!container || container.querySelector('#testButton')) return;
@@ -14,6 +64,8 @@
                 </div>
             </div>
         `);
+
+        document.querySelector('#testButton').onclick = openModal;
     }
 
     new MutationObserver(inject).observe(document.body, { childList: true, subtree: true });
