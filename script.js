570c8446c85d66a5cf5e530bf8f716d01b89fb07
Hugo <hugo.leveque@efrei.net>
2026-06-21 21:16:18 +0200

    update

--- script.js
+++ script.js
@@ -66,19 +66,22 @@
 
     function inject() {
         var container = document.querySelector('._3cykd-VfN_xBxf3Qxriccm._1-9sir4j_KQiMqdkZjQN0u');
-        if (!container || container.querySelector('#testButton')) return;
+        if (!container || container.querySelector('#addGameButton')) return;
 
         container.insertAdjacentHTML('afterbegin', `
-            <div id="testButton" style="cursor:pointer; -webkit-app-region:no-drag; margin-right:8px;">
+        <div style="-webkit-app-region:no-drag; display:flex; align-items:center; gap:8px; margin-right:8px;">
+            <div class="_2Lu3d-5qLmW4i19ysTt2jT"><div class="">OpenSteam v1.2</div></div>
+            <div id="addGameButton" style="cursor:pointer;">
                 <div class="_2Szzh5sKyGgnLUR870zbDE RtSv39ZoBOySnb8XQ5hJf _3qhGkQ5qLVNQQ-J2-uPoHt">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" width="18" height="18" fill="none" style="width:18px !important; height:18px !important; display:block; flex:0 0 auto;" class="_34bQcTHo5QKzuujoEyU1tm">
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M20 6H16V16H6V20H16V30H20V20H30V16H20V6Z" fill="currentColor"/>
                     </svg>
                 </div>
             </div>
-        `);
+        </div>
+    `);
 
-        document.querySelector('#testButton').onclick = openModal;
+        document.querySelector('#addGameButton').onclick = openModal;
     }
 
     new MutationObserver(inject).observe(document.body, { childList: true, subtree: true });
