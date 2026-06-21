aff0f6d50eb1b62bb39105c8cee02d16156a29d2
Hugo <hugo.leveque@efrei.net>
2026-06-21 21:05:06 +0200

    update

--- script.js
+++ script.js
@@ -23,22 +23,20 @@
                             </div>
                             <div class="DialogContent _DialogLayout GenericDialogBase GenericConfirmDialog _DialogCenterVertically" style="min-width:500px;">
                                 <div class="DialogContent_InnerWidth">
-                                    <div role="dialog">
-                                        <div role="heading" aria-level="2" class="DialogHeader">Install a script</div>
-                                        <div class="DialogBody">
-                                            <div class="DialogInput_Wrapper _DialogLayout Panel">
-                                                <input id="os-appid-input"
-                                                    class="DialogInput DialogInputPlaceholder DialogTextInputBase Focusable"
-                                                    placeholder="AppID"
-                                                    type="text"
-                                                    spellcheck="false">
-                                            </div>
+                                    <div role="heading" aria-level="2" class="DialogHeader">Install a script</div>
+                                    <div class="DialogBody">
+                                        <div class="DialogInput_Wrapper _DialogLayout Panel">
+                                            <input id="os-appid-input"
+                                                class="DialogInput DialogInputPlaceholder DialogTextInputBase Focusable"
+                                                placeholder="AppID"
+                                                type="text"
+                                                spellcheck="false">
                                         </div>
-                                        <div class="DialogFooter">
-                                            <div class="DialogTwoColLayout _DialogColLayout Panel">
-                                                <button id="os-download-btn" type="button" class="DialogButton _DialogLayout Primary Focusable">Download</button>
-                                                <button id="os-cancel-btn" type="button" class="DialogButton _DialogLayout Secondary Focusable">Cancel</button>
-                                            </div>
+                                    </div>
+                                    <div class="DialogFooter">
+                                        <div class="DialogTwoColLayout _DialogColLayout Panel">
+                                            <button id="os-download-btn" type="button" class="DialogButton _DialogLayout Primary Focusable">Download</button>
+                                            <button id="os-cancel-btn" type="button" class="DialogButton _DialogLayout Secondary Focusable">Cancel</button>
                                         </div>
                                     </div>
                                 </div>
@@ -53,10 +51,8 @@
         container.appendChild(wrapper);
 
         var closeModal = () => wrapper.remove();
-        var dialogContent = wrapper.querySelector('.DialogContent');
-
         wrapper.querySelector('.ModalOverlayContent.active').addEventListener('click', (e) => {
-            if (!dialogContent.contains(e.target)) closeModal();
+            if (!wrapper.querySelector('.DialogContent').contains(e.target)) closeModal();
         });
         wrapper.querySelector('#os-close-btn').onclick = closeModal;
         wrapper.querySelector('#os-cancel-btn').onclick = closeModal;
@@ -74,12 +70,10 @@
 
         container.insertAdjacentHTML('afterbegin', `
             <div id="testButton" style="cursor:pointer; -webkit-app-region:no-drag; margin-right:8px;">
-                <div>
-                    <div class="_2Szzh5sKyGgnLUR870zbDE RtSv39ZoBOySnb8XQ5hJf _3qhGkQ5qLVNQQ-J2-uPoHt">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" width="18" height="18" fill="none" style="width:18px !important; height:18px !important; display:block; flex:0 0 auto;" class="_34bQcTHo5QKzuujoEyU1tm">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M20 6H16V16H6V20H16V30H20V20H30V16H20V6Z" fill="currentColor"/>
-                        </svg>
-                    </div>
+                <div class="_2Szzh5sKyGgnLUR870zbDE RtSv39ZoBOySnb8XQ5hJf _3qhGkQ5qLVNQQ-J2-uPoHt">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" width="18" height="18" fill="none" style="width:18px !important; height:18px !important; display:block; flex:0 0 auto;" class="_34bQcTHo5QKzuujoEyU1tm">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M20 6H16V16H6V20H16V30H20V20H30V16H20V6Z" fill="currentColor"/>
+                    </svg>
                 </div>
             </div>
         `);
