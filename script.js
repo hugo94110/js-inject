e1652ac0e6aeb7e6bd0905f505497568e0b44f9d
Hugo <hugo.leveque@efrei.net>
2026-07-01 14:19:00 +0200

    update

--- script.js
+++ script.js
@@ -1,176 +1,3 @@
-// (function() {
-//     const title = document.title;
-
-//     if (title === 'Steam') {
-//         function openAddGameModal() {
-//             if (document.querySelector('#addGameModal')) return;
-
-//             var wrapper = document.createElement('div');
-//             wrapper.id = 'addGameModal';
-//             wrapper.className = 'FullModalOverlay';
-//             wrapper.innerHTML = `
-//                 <div class="ModalOverlayContent ModalOverlayBackground"></div>
-//                 <dialog style="all:unset; display:contents;" open="">
-//                     <div></div>
-//                     <div class="ModalOverlayContent active" tabindex="-1">
-//                         <div class="ModalPosition" tabindex="0">
-//                             <div class="ModalPosition_Content">
-//                                 <div class="ModalPosition_TopBar"></div>
-//                                 <div class="ModalPosition_Dismiss">
-//                                     <div id="addGameModalCloseButton" class="closeButton">
-//                                         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" class="SVGIcon_Button SVGIcon_X_Line" width="256px" height="256px" viewBox="0 0 256 256">
-//                                             <line fill="none" stroke="#FFFFFF" stroke-width="45" stroke-miterlimit="10" x1="212" y1="212" x2="44" y2="44"></line>
-//                                             <line fill="none" stroke="#FFFFFF" stroke-width="45" stroke-miterlimit="10" x1="44" y1="212" x2="212" y2="44"></line>
-//                                         </svg>
-//                                     </div>
-//                                 </div>
-//                                 <div class="DialogContent _DialogLayout GenericDialogBase GenericConfirmDialog _DialogCenterVertically" style="min-width:500px;">
-//                                     <div class="DialogContent_InnerWidth">
-//                                         <div role="heading" aria-level="2" class="DialogHeader">Add a game</div>
-//                                         <div class="DialogBody">
-//                                             <div class="DialogBodyText">
-//                                                 <div id="«r26b»" style="display: contents;">
-//                                                     Add a game to your library by entering its AppID. You can find every AppID's on <a href="#">https://steamdb.info</a><br><br>
-//                                                 </div>
-//                                             <div class="DialogInput_Wrapper _DialogLayout Panel">
-//                                                 <input id="addGameModalAppIDInput"
-//                                                     class="DialogInput DialogInputPlaceholder DialogTextInputBase Focusable"
-//                                                     placeholder="Enter an AppID..."
-//                                                     type="text"
-//                                                     spellcheck="false">
-//                                             </div>
-//                                         </div>
-//                                         <div class="DialogFooter">
-//                                             <div class="DialogTwoColLayout _DialogColLayout Panel">
-//                                                 <button id="addGameModalDownloadButton" type="button" class="DialogButton _DialogLayout Primary Focusable">Download</button>
-//                                                 <button id="addGameModalCancelButton" type="button" class="DialogButton _DialogLayout Secondary Focusable">Cancel</button>
-//                                             </div>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </dialog>
-//             `;
-
-//             var container = document.querySelector('._27qasW5wLU4h4nUgawpo1q');
-//             if (!container) return;
-//             container.appendChild(wrapper);
-
-//             function closeAddGameModal() {
-//                 wrapper.remove();
-//             }
-
-//             wrapper.querySelector('.ModalOverlayContent.active').addEventListener('click', function(e) {
-//                 if (!wrapper.querySelector('.DialogContent').contains(e.target)) {
-//                     closeAddGameModal();
-//                 }
-//             });
-
-//             wrapper.querySelector('#addGameModalCloseButton').onclick = closeAddGameModal;
-//             wrapper.querySelector('#addGameModalCancelButton').onclick = closeAddGameModal;
-            
-//             wrapper.querySelector('#addGameModalDownloadButton').onclick = async function() {
-//                 var appid = wrapper.querySelector('#addGameModalAppIDInput').value.trim();
-//                 if (!appid) return;
-
-//                 try {
-//                     const res = await fetch('http://localhost:9223/download?appid=' + appid);
-//                     const data = await res.json();
-//                     if (data.ok) {
-//                         closeAddGameModal();
-//                     } else {
-//                         console.error('error : ', data.error);
-//                     }
-//                 } catch(e) {
-//                     console.error('server unreachable : ', e);
-//                 }
-//             };
-//         }
-
-//         function injectS() {
-//             var steamLabel = document.querySelector('.bSKGlAJG2UVWTsntEJY2v');
-//             if (steamLabel) {
-//                 var textNode = steamLabel.childNodes[steamLabel.childNodes.length - 1];
-//                 if (textNode && textNode.nodeType === Node.TEXT_NODE && textNode.textContent !== 'OpenSteam') {
-//                     textNode.textContent = 'OpenSteam';
-//                 }
-//             }
-
-//             var rootBarButtonContainer = document.querySelector('._3cykd-VfN_xBxf3Qxriccm._1-9sir4j_KQiMqdkZjQN0u');
-//             if (!rootBarButtonContainer) return;
-
-//             if (!rootBarButtonContainer.querySelector('#githubButton')) {
-//                 rootBarButtonContainer.insertAdjacentHTML('afterbegin', `
-//                     <div id="githubButton" style="cursor:pointer;">
-//                         <div class="_2Szzh5sKyGgnLUR870zbDE _5wILZhsLODVwGfcJ0hKmJ" style="display:flex; align-items:center; justify-content:center;">
-//                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" style="width:16px !important; height:16px !important; display:block; flex:0 0 auto; margin:auto;" class="_34bQcTHo5QKzuujoEyU1tm">
-//                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" fill="currentColor"/>
-//                             </svg>
-//                         </div>
-//                     </div>
-//                 `);
-
-//                 document.querySelector('#githubButton').onclick = function(event) {
-//                     event.preventDefault();
-//                     var link = document.createElement('a');
-//                     link.href = 'https://github.com/hugo94110/OpenSteam';
-//                     link.target = '_blank';
-//                     link.rel = 'noopener noreferrer';
-//                     link.click();
-//                 };
-//             }
-
-//             if (!document.querySelector('#customStyles')) {
-//                 var style = document.createElement('style');
-//                 style.id = 'customStyles';
-//                 style.textContent = [
-//                     '#addGameButton .SVGIcon_Arrow path { fill: rgb(169, 169, 169); }',
-//                     '#addGameButton:hover .SVGIcon_Arrow path { fill: rgb(255, 255, 255); }'
-//                 ].join(' ');
-//                 document.head.appendChild(style);
-//             }
-
-//             var addGameContainer = document.querySelector('._2WgQEFvIzJw_SHNGbjtRFU');
-//             if (addGameContainer && !document.querySelector('#addGameButton')) {
-//                 addGameContainer.insertAdjacentHTML('beforeend', `
-//                     <div id="addGameButton" class="uE7Pj4tb2n3_Bx4vjEX0a rkfSfuCLRt8sqpkXJqxYo Focusable" tabindex="0" role="button">
-//                         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" class="SVGIcon_Button SVGIcon_Arrow" x="0px" y="0px" width="256px" height="256px" viewBox="0 0 256 256">
-//                             <path transform="scale(10.667)" d="M20,11h-7V4c0-0.552-0.448-1-1-1s-1,0.448-1,1v7H4c-0.552,0-1,0.448-1,1s0.448,1,1,1h7v7c0,0.552,0.448,1,1,1s1-0.448,1-1 v-7h7c0.552,0,1-0.448,1-1S20.552,11,20,11z"></path>
-//                         </svg>
-//                     </div>
-//                 `);
-//                 document.querySelector('#addGameButton').onclick = openAddGameModal;
-//             }
-//         }
-
-//         new MutationObserver(injectS).observe(document.body, { childList: true, subtree: true });
-//         injectS();
-//     }
-
-//     if (title === 'Steam Root Menu') {
-//         function injectSTM() {
-//             var container = document.querySelector('._2EstNjFIIZm_WUSKm5Wt7n._3pofGqV0buiKAfMPEs3_82');
-//             if (!container) return;
-//             if (container.querySelector('#rootMenuOSSItem')) return;
-
-//             var referenceNode = container.children[container.children.length - 2];
-//             var temp = document.createElement('div');
-//             temp.innerHTML = `
-//                 <hr class="_2jXHP0742MyApMUVUM8IFn _21GPYlKBCLsHQpTsHw_RL_">
-//                 <div id="rootMenuOSSItem" role="menuitem" class="_2jXHP0742MyApMUVUM8IFn _2uiDecKkKjAq7nimy3uLhG _1n7Wloe5jZ6fSuvV18NNWI contextMenuItem">OpenSteam Settings</div>
-//             `;
-//             while (temp.firstChild) {
-//                 container.insertBefore(temp.firstChild, referenceNode);
-//             }
-//         }
-
-//         new MutationObserver(injectSTM).observe(document.body, { childList: true, subtree: true });
-//         injectSTM();
-//     }
-// })();
-
 (function() {
     function openModal() {
         if (document.querySelector('#os-modal')) return;
@@ -220,24 +47,16 @@
             </dialog>
         `;
 
-        var container = document.querySelector('._27qasW5wLU4h4nUgawpo1q');
-        if (!container) container = document.body;
+        var container = document.querySelector('._27qasW5wLU4h4nUgawpo1q') || document.body;
         container.appendChild(wrapper);
 
-        function closeModal() {
-            wrapper.remove();
-        }
-
-        wrapper.querySelector('.ModalOverlayContent.active').addEventListener('click', function(e) {
-            if (!wrapper.querySelector('.DialogContent').contains(e.target)) {
-                closeModal();
-            }
+        var closeModal = () => wrapper.remove();
+        wrapper.querySelector('.ModalOverlayContent.active').addEventListener('click', (e) => {
+            if (!wrapper.querySelector('.DialogContent').contains(e.target)) closeModal();
         });
-
         wrapper.querySelector('#os-close-btn').onclick = closeModal;
         wrapper.querySelector('#os-cancel-btn').onclick = closeModal;
-        
-        wrapper.querySelector('#os-download-btn').onclick = function() {
+        wrapper.querySelector('#os-download-btn').onclick = () => {
             var appid = wrapper.querySelector('#os-appid-input').value.trim();
             if (!appid) return;
             console.log('OpenSteam: download for AppID', appid);
@@ -247,19 +66,38 @@
 
     function inject() {
         var container = document.querySelector('._3cykd-VfN_xBxf3Qxriccm._1-9sir4j_KQiMqdkZjQN0u');
-        if (!container) return;
+        if (!container || container.querySelector('#addGameButton') || container.querySelector('#githubButton')) return;
 
-        if (!container.querySelector('#githubButton')) {
-            container.insertAdjacentHTML('afterbegin', `
+        container.insertAdjacentHTML('afterbegin', `
+            <div style="-webkit-app-region:no-drag; display:flex; align-items:center; gap:8px; margin-right:8px;">
+                <div class="_2Lu3d-5qLmW4i19ysTt2jT"><div class="" style="font-size:13px;">OpenSteam v1.2.0</div></div>
+
                 <div id="githubButton" style="cursor:pointer;">
-                    <div class="_2Szzh5sKyGgnLUR870zbDE _5wILZhsLODVwGfcJ0hKmJ" style="display:flex; align-items:center; justify-content:center;">
+                    <div class="_2Szzh5sKyGgnLUR870zbDE RtSv39ZoBOySnb8XQ5hJf _3qhGkQ5qLVNQQ-J2-uPoHt" style="display:flex; align-items:center; justify-content:center;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" style="width:16px !important; height:16px !important; display:block; flex:0 0 auto; margin:auto;" class="_34bQcTHo5QKzuujoEyU1tm">
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" fill="currentColor"/>
                         </svg>
                     </div>
                 </div>
-            `);
-            document.querySelector('#githubButton').onclick = function(event) {
+
+                <!-- <div id="addGameButton" style="cursor:pointer;">
+                    <div class="_2Szzh5sKyGgnLUR870zbDE RtSv39ZoBOySnb8XQ5hJf _3qhGkQ5qLVNQQ-J2-uPoHt">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" width="18" height="18" fill="none" style="width:18px !important; height:18px !important; display:block; flex:0 0 auto;" class="_34bQcTHo5QKzuujoEyU1tm">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M20 6H16V16H6V20H16V30H20V20H30V16H20V6Z" fill="currentColor"/>
+                        </svg>
+                    </div>
+                </div> -->
+            </div>
+        `);
+
+        var addGameButton = document.querySelector('#addGameButton');
+        if (addGameButton) {
+            addGameButton.onclick = openModal;
+        }
+
+        var githubButton = document.querySelector('#githubButton');
+        if (githubButton) {
+            githubButton.onclick = function(event) {
                 event.preventDefault();
                 var link = document.createElement('a');
                 link.href = 'https://github.com/hugo94110';
@@ -267,19 +105,6 @@
                 link.rel = 'noopener noreferrer';
                 link.click();
             };
-        }
-
-        var addGameContainer = document.querySelector('._2WgQEFvIzJw_SHNGbjtRFU');
-        if (addGameContainer && !document.querySelector('#addGameButton')) {
-            addGameContainer.insertAdjacentHTML('beforeend', `
-                <div id="addGameButton" class="uE7Pj4tb2n3_Bx4vjEX0a rkfSfuCLRt8sqpkXJqxYo Focusable" tabindex="0" role="button">
-                    <svg version="1.1" id="arrowBase" xmlns="http://www.w3.org/2000/svg" class="SVGIcon_Button SVGIcon_Arrow" x="0px" y="0px" width="256px" height="256px" viewBox="0 0 256 256" style="transform: rotate(0deg);">
-                        <polyline fill="none" stroke="#ffffff" stroke-width="10" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="128,247.688 128,8.313 181.061,61.674 "></polyline>
-                        <polyline fill="none" stroke="#ffffff" stroke-width="10" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="128,247.688 128,8.313 74.939,61.674 "></polyline>
-                    </svg>
-                </div>
-            `);
-            document.querySelector('#addGameButton').onclick = openModal;
         }
     }
 
