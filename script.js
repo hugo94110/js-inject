4e139b09e224ad74e3f0a605d19d1a06d48a3cc3
Hugo <hugo.leveque@efrei.net>
2026-07-05 12:59:43 +0200

    update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
     const title = document.title;
     const sharedContext = window.opener;
 
+    // External functions
     function getMainWindow() {
         if (title === 'Steam') return window;
         try {
@@ -214,47 +215,43 @@
         };
     }
 
+
+    // Main logic
     if (title === 'Steam') {
-        function injectS() {
-            var steamLabel = document.querySelector('.bSKGlAJG2UVWTsntEJY2v');
-            if (steamLabel) {
-                var textNode = steamLabel.childNodes[steamLabel.childNodes.length - 1];
-                if (textNode && textNode.nodeType === Node.TEXT_NODE && textNode.textContent !== 'OpenSteam') {
-                    textNode.textContent = 'OpenSteam';
+        function injectWindow(win) {
+            var doc = win.document;
+
+            if (doc.title === 'Steam') {
+                var steamLabel = doc.querySelector('.bSKGlAJG2UVWTsntEJY2v');
+                if (steamLabel) {
+                    var textNode = steamLabel.childNodes[steamLabel.childNodes.length - 1];
+                    if (textNode && textNode.nodeType === Node.TEXT_NODE && textNode.textContent !== 'OpenSteam') {
+                        textNode.textContent = 'OpenSteam';
+                    }
+                }
+
+                var rootBarButtonContainer = doc.querySelector('._3cykd-VfN_xBxf3Qxriccm._1-9sir4j_KQiMqdkZjQN0u');
+                if (rootBarButtonContainer && !rootBarButtonContainer.querySelector('#githubButton')) {
+                    rootBarButtonContainer.insertAdjacentHTML('afterbegin', `
+                        <div id="githubButton" style="cursor:pointer;">
+                            <div class="_2Szzh5sKyGgnLUR870zbDE _5wILZhsLODVwGfcJ0hKmJ" style="display:flex; align-items:center; justify-content:center;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" style="width:16px !important; height:16px !important; display:block; flex:0 0 auto; margin:auto;" class="_34bQcTHo5QKzuujoEyU1tm">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" fill="currentColor"/>
+                                </svg>
+                            </div>
+                        </div>
+                    `);
+
+                    doc.querySelector('#githubButton').onclick = function(event) {
+                        event.preventDefault();
+                        var link = doc.createElement('a');
+                        link.href = 'https://github.com/hugo94110/OpenSteam';
+                        link.target = '_blank';
+                        link.rel = 'noopener noreferrer';
+                        link.click();
+                    };
                 }
             }
-
-            var rootBarButtonContainer = document.querySelector('._3cykd-VfN_xBxf3Qxriccm._1-9sir4j_KQiMqdkZjQN0u');
-            if (!rootBarButtonContainer) return;
-
-            if (!rootBarButtonContainer.querySelector('#githubButton')) {
-                rootBarButtonContainer.insertAdjacentHTML('afterbegin', `
-                    <div id="githubButton" style="cursor:pointer;">
-                        <div class="_2Szzh5sKyGgnLUR870zbDE _5wILZhsLODVwGfcJ0hKmJ" style="display:flex; align-items:center; justify-content:center;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" style="width:16px !important; height:16px !important; display:block; flex:0 0 auto; margin:auto;" class="_34bQcTHo5QKzuujoEyU1tm">
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z" fill="currentColor"/>
-                            </svg>
-                        </div>
-                    </div>
-                `);
-
-                document.querySelector('#githubButton').onclick = function(event) {
-                    event.preventDefault();
-                    var link = document.createElement('a');
-                    link.href = 'https://github.com/hugo94110/OpenSteam';
-                    link.target = '_blank';
-                    link.rel = 'noopener noreferrer';
-                    link.click();
-                };
-            }
-
-        }
-
-        new MutationObserver(injectS).observe(document.body, { childList: true, subtree: true });
-        injectS();
-
-        function injectMenuItems(win) {
-            var doc = win.document;
 
             if (doc.title === 'Steam Root Menu') {
                 var container = doc.querySelector('._2EstNjFIIZm_WUSKm5Wt7n._3pofGqV0buiKAfMPEs3_82');
@@ -292,11 +289,10 @@
         function watchPopup(popup) {
             try {
                 var win = popup.m_popup.window || popup.m_popup;
-                if (win === window) return;
                 var attach = function() {
                     if (!win.document || !win.document.body) { setTimeout(attach, 10); return; }
-                    new win.MutationObserver(function() { injectMenuItems(win); }).observe(win.document.body, { childList: true, subtree: true });
-                    injectMenuItems(win);
+                    new win.MutationObserver(function() { injectWindow(win); }).observe(win.document.body, { childList: true, subtree: true });
+                    injectWindow(win);
                 };
                 attach();
             } catch (e) {}
@@ -305,6 +301,8 @@
         try {
             Array.from(sharedContext.g_PopupManager.GetPopups()).forEach(watchPopup);
             sharedContext.g_PopupManager.AddPopupCreatedCallback(watchPopup);
-        } catch (e) {}
+        } catch (e) {
+            watchPopup({ m_popup: window });
+        }
     }
 })();
